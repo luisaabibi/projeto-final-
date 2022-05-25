4a37c325002512e857990b2fedc4535adc63895c
luisaabibi <lubiabibi@gmail.com>
2022-05-25 11:16:27 -0300

    adicionar cogumelos na posição e velocidade corretas

--- jogo.py
+++ jogo.py
@@ -1,5 +1,8 @@
+import random
 import py
-import pygame 
+import pygame
+
+from sprites import Cogumelo 
 pygame.init()
 
 WIDTH = 1200
@@ -16,24 +19,37 @@ game= True
 image = pygame.image.load('assets/img/fundo2.jpg').convert()
 image=pygame.transform.scale(image, (2400, HEIGHT))
 
+cogumelo_img= pygame.image.load('assets/img/cogumelo.png').convert_alpha()
+cogumelo_img= pygame.transform.scale(cogumelo_img, (40, 40))
+
+all_sprites = pygame.sprite.Group()
+all_cogumelos = pygame.sprite.Group()
 
 i=0
-
 while game :
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT :
             game = False 
 
-    window.fill((0, 0, 0))  # Preenche com a cor branca]
+
+    if len(all_cogumelos) < 10:
+        cogumelo = Cogumelo(cogumelo_img, WIDTH + random.randint(0, 1000), 400)
+        all_cogumelos.add(cogumelo)
+        all_sprites.add(cogumelo)
 
 
+    all_sprites.update()
+
+    window.fill((0, 0, 0))  # Preenche com a cor branca
     window.blit(image, (i, 0))
 
-    if i == -1200:
+    if i == -WIDTH:
         i=0
     else:
         i-=5
+
+    all_sprites.draw(window)
     pygame.display.update()
 
 pygame.quit()
