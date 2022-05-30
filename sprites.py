20741853f8459cc5cbd5f6b3dcd2f7b9a15d9eb8
Nicholaskapaz <104779932+Nicholaskapaz@users.noreply.github.com>
2022-05-30 17:31:45 -0300

    sprites cobra e pedra, correção do distanciamento das sprites

--- sprites.py
+++ sprites.py
@@ -1,8 +1,8 @@
 import pygame
 
 class Cogumelo(pygame.sprite.Sprite):
-    def __init__(self, img, x, y):
-        pygame.sprite.Sprite.__init__(self)
+    def _init_(self, img, x, y):
+        pygame.sprite.Sprite._init_(self)
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -15,18 +15,38 @@ class Cogumelo(pygame.sprite.Sprite):
             self.kill()
 
 class Pedra(pygame.sprite.Sprite):
-    def __init__(self, img, x, y):
-        pygame.sprite.Sprite.__init__(self)
+    def _init_(self, img, x, y):
+        pygame.sprite.Sprite._init_(self)
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.speedx = -5
+    
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.x < -50:
+            self.kill()
+
+class Cobra(pygame.sprite.Sprite):
+    def _init_(self, img, x, y):
+        pygame.sprite.Sprite._init_(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speedx = -5
+    
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.x < -50:
+            self.kill()
 
 
-class fox(pygame.sprite.Sprite):
-    def __init__(self, assets, x, y):
-        pygame.sprite.Sprite.__init__(self)
+
+class Fox(pygame.sprite.Sprite):
+    def _init_(self, assets, x, y):
+        pygame.sprite.Sprite._init_(self)
         self.image = assets['raposa2']
         self.rect = self.image.get_rect()
         self.rect.x = 100
