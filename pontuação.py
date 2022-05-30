5b500f1b376526db8e99822b834ad0329f9c265d
Bruno0nishi <brunoo2@al.insper.edu.br>
2022-05-30 16:32:51 -0300

    inicio sistema de pontuação

---
+++ pontuação.py
@@ -0,0 +1,11 @@
+score_value=0
+font=pygame.font.Font('freesansbold.ttf',32)
+textX = 10
+testy = 10
+def show_score(x,y):
+    score=font.render("Score:"+str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+
+
+
+show_score(textX,testy)
