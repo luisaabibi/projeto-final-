3003351081f80b4eca2222b5851f299897b00393
Bruno0nishi <brunoo2@al.insper.edu.br>
2022-06-07 17:22:47 -0300

    sprites

--- sprites.py
+++ sprites.py
@@ -45,16 +45,32 @@ class Cobra(pygame.sprite.Sprite):
 
 
 class Fox(pygame.sprite.Sprite):
+    x = 150
+    y = 295
+    
     def __init__(self, img,x, y):
         pygame.sprite.Sprite.__init__(self)
+        self.fox_padrao = True
+        self.fox_pulando = False
+
+        self.step_index = 0
         self.image = img
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.speedy = 0
+        self.rect.x = self.x
+        self.rect.y = self.y
+
     def update(self):
-        self.rect.y += self.speedy
-        if self.rect.y < -50:
-            self.kill()
+        if self.fox_pulando:
+            self.pular()
+    def pular(self):
+        self.rect.y -= 10
+        if self.fox_pulando:
+            self.rect.y -= self.v_jump * 3
+            self.v_jump -= 0.8
+        if self.rect.y >= self.y:
+            self.fox_pulando = False
+            self.rect.y = self.y
+
        
+
        
